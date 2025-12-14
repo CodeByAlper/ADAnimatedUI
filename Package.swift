ef8b5862f18b0872682011da1e0358bfbe3c532e
Alper D. <aad@doppelde.de>
2025-12-14 19:12:13 +0100

    feat(package): add support for watchOS and tvOS

--- Package.swift
+++ Package.swift
@@ -7,6 +7,8 @@ let package = Package(
     platforms: [
         .iOS(.v17),
         .macOS(.v14),
+        .watchOS(.v10),
+        .tvOS(.v17)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
